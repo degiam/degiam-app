1291ecfeb020d1ef98b80f96d5f3088957885d84
degiam <degiam@ymail.com>
2025-01-10 00:21:42 +0700

    update: home

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -22,6 +22,7 @@ export class FooterComponent {
 
   get years(): string {
     const currentYear = new Date().getFullYear();
-    return currentYear === 2024 ? '2024' : `2024-${currentYear}`;
+    // return currentYear === 2024 ? '2024' : `2024-${currentYear}`;
+    return String(currentYear);
   }
 }

f4a83ff7be57a0ae5ec9eae22f278ad00789d720
degiam <degiam@ymail.com>
2025-01-18 09:59:40 +0700

    add: pwa support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideRouter } from '@angular/router';
+import { isDevMode } from '@angular/core';
 import { appConfig } from '@/app.config';
 import { AppComponent } from '@/app.component';
+import { routes } from '@/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+const platformApp = bootstrapApplication(AppComponent, {
+  providers: [
+    {
+      provide: 'AppConfig',
+      useValue: appConfig
+    },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideRouter(routes),
+  ],
+});
+
+platformApp
   .catch((err) => console.error(err));
